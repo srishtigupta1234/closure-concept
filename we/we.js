ede83e1468f25b0aad9d38d4bada974037b354da
srishtigupta1234 <srishtigupta97527@gmail.com>
2024-09-06 10:00:03 +0530

    commiting changes

--- we/we.js
+++ we/we.js
@@ -1,4 +1,5 @@
 let id;
+let container;
 let movie_div = document.getElementById("movie")
 
 async function searchMovies(q){
@@ -28,10 +29,29 @@ function appendMovie(movies){
       return false;
     }
     movies.forEach(function(el){
+        let head = document.createElement("h1")
+        head.innerText = "Movies"
         let p = document.createElement("p");
         p.innerText = el.Title;
+        p.addEventListener("click",function(){
+            appendDetails(el);
+        })
         movie_div.append(p);
     })
+}
+
+function appendDetails(el){
+    let container = document.getElementById("container")
+    container.innerHTML = null;
+    let poster = document.createElement("img")
+    poster.setAttribute("src",el.Poster);
+    let name = document.createElement("h1")
+    name.innerText = el.Title;
+    let year = document.createElement("h2")
+    year.innerText = `Released on : ${el.Year}`
+    let type = document.createElement("h3")
+    type.innerText = `Type : ${el.Type}`;
+    container.append(poster,name,year,type);
 }
 function debounceFunction(func,delay){
     if(id){
